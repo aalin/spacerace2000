406e90e21b13387dd7d2fa43470647d65195bb1b
Andreas Alin <andreas.alin@gmail.com>
2013-06-25 23:15:14 +0700

    Remove unused opengl includes

--- opengl.hpp
+++ opengl.hpp
@@ -6,12 +6,8 @@
 
 #ifdef __APPLE__
 #include <OpenGL/gl.h>
-#include <OpenGL/glext.h>
-#include <OpenGL/glu.h>
 #else
 #include <GL/gl.h>
-#include <GL/glext.h>
-#include <GL/glu.h>
 #endif
 
 void _glPrintErrors(const char *file, int line);
