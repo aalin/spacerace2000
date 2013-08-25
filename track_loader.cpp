2561911b7c051834b389dd3af44cdb41f19856ef
Andreas Alin <andreas.alin@gmail.com>
2013-08-25 14:54:32 +0200

    Cleanup

--- track_loader.cpp
+++ track_loader.cpp
@@ -10,17 +10,18 @@ TrackLoader::TrackLoader(std::vector<glm::vec3> key_points, unsigned int detail,
 }
 
 TrackLoader::SectionRect TrackLoader::generateSectionRect(unsigned int i) const {
-	glm::vec3 current(getPoint(i));;
-	glm::vec3 next(getPoint(i + 1));
+	const glm::vec3 current(getPoint(i));;
+	const glm::vec3 next(getPoint(i + 1));
 
-	glm::vec3 delta(current - next);
+	const glm::vec3 delta(current - next);
 
-	glm::vec3 outwards(glm::normalize(glm::vec3(delta.y, -delta.x, 0)));
+	const glm::vec3 outwards(glm::normalize(glm::vec3(delta.y, -delta.x, 0)));
+	const glm::vec3 height = glm::vec3(0.0, 0.0, _height);
 
 	glm::vec3 top_left(current + outwards * -_width);
 	glm::vec3 top_right(current + outwards * _width);
-	glm::vec3 bottom_left(top_left - glm::vec3(0.0, 0.0, _height));
-	glm::vec3 bottom_right(top_right - glm::vec3(0.0, 0.0, _height));
+	glm::vec3 bottom_left(top_left - height);
+	glm::vec3 bottom_right(top_right - height);
 
 	return SectionRect{ top_left, top_right, bottom_left, bottom_right };
 }
