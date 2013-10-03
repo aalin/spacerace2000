e1e10f8c0bdd33dbf6a106841a815a4bd532310c
Andreas Alin <andreas.alin@gmail.com>
2013-10-03 22:23:26 +0200

    yeeeah, use matrices and stuff

--- track_loader.cpp
+++ track_loader.cpp
@@ -1,5 +1,6 @@
 #include "track_loader.hpp"
 #include "splines.hpp"
+#include <glm/gtc/matrix_transform.hpp>
 
 TrackLoader::TrackLoader(std::vector<glm::vec3> key_points, unsigned int detail, float width, float height)
 	: _width(width), _height(height) {
@@ -9,19 +10,40 @@ TrackLoader::TrackLoader(std::vector<glm::vec3> key_points, unsigned int detail,
 		_section_rects.push_back(generateSectionRect(i));
 }
 
+float magnitude(const glm::vec2 v) {
+	return std::sqrt(v.x * v.x + v.y * v.y);
+}
+
+float calculateDegrees(const glm::vec3 prev, const glm::vec3 curr, const glm::vec3 next) {
+	return 0.0;
+}
+
 TrackLoader::SectionRect TrackLoader::generateSectionRect(unsigned int i) const {
 	const glm::vec3 current(getPoint(i));;
 	const glm::vec3 next(getPoint(i + 1));
 
 	const glm::vec3 delta(current - next);
+	const glm::vec3 forward(glm::normalize(delta));
 
 	const glm::vec3 outwards(glm::normalize(glm::vec3(delta.y, -delta.x, 0)));
-	const glm::vec3 height = glm::vec3(0.0, 0.0, _height);
+	const glm::vec4 height(0.0, 0.0, _height, 0.0);
 
-	glm::vec3 top_left(current + outwards * -_width);
-	glm::vec3 top_right(current + outwards * _width);
-	glm::vec3 bottom_left(top_left - height);
-	glm::vec3 bottom_right(top_right - height);
+	glm::vec4 top_left(outwards * -_width, 1.0);
+	glm::vec4 top_right(outwards * _width, 1.0);
+	glm::vec4 bottom_left(top_left - height);
+	glm::vec4 bottom_right(top_right - height);
 
-	return SectionRect{ top_left, top_right, bottom_left, bottom_right };
+	float degrees = calculateDegrees(getPoint(i - 1), current, next);
+
+	glm::mat4 translation = glm::translate(glm::mat4(1.0), current);
+	glm::mat4 rotation = glm::rotate(glm::mat4(1.0), degrees, forward);
+
+	glm::mat4 final = translation * rotation;
+
+	return SectionRect{
+		glm::vec3(final * top_left),
+		glm::vec3(final * top_right),
+		glm::vec3(final * bottom_left),
+		glm::vec3(final * bottom_right)
+	};
 }
